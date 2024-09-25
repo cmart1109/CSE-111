654aec4b9be2d1b01f62aa97a7bc2b040f420991
cmart1109 <christianandremartinez@gmail.com>
2024-09-25 16:44:43 -0600

    surfaces

--- storage_efficiency.py
+++ storage_efficiency.py
@@ -1,11 +1,26 @@
 import math;
 pi = math.pi
 def main():
-    #volume = compute_volume(6.83, 10.16)
-    #surface_area = compute_surface_area
-    Picnic = storage_efficiency(compute_volume(6.83, 10.16), compute_surface_area(6.83, 10.16))
-    print(f'#1 Picnic {Picnic}')
+    calculate("#1 Picnic", 6.83, 10.16)
+    calculate("#1 Tall", 7.78, 11.91)
+    calculate("#2", 8.73, 11.59)
+    calculate("#2.5", 10.32, 11.91)
+    calculate("#3 Cylinder", 10.79, 17.78)
+    calculate("#5", 13.02, 14.29)
+    calculate("#6Z", 5.40, 8.89)
+    calculate("#BZ Short", 6.83, 7.62)
+    calculate("#10", 15.72, 17.78)
+    calculate("#211", 6.83, 12.38)
+    calculate("#300", 7.62, 11.27)
+    calculate("#303", 8.10, 11.11)
+
 pass
+def calculate(name, radius, height):
+        volume = compute_volume(radius, height)
+        surface = compute_surface_area(radius, height)
+        storage = storage_efficiency(volume, surface)
+        storage = round(storage, 2)
+        print (f"{name},  {storage}")
 def compute_volume(radius, height):
         volume = pi * radius **2 * height
         return volume
